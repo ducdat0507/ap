dcd9c7ce09aa776ded83d1a7b14c655f24736d19
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2025-03-11 23:00:42 +0700

    Project3

--- Project3/Project3/Library.cs
+++ Project3/Project3/Library.cs
@@ -49,6 +49,63 @@ namespace Project3
             }
         }
 
+        public void ListAllBooks()
+        {
+            Console.WriteLine("Listing all books");
+            ListBooks(Books);
+        }
+
+        public void SearchByName()
+        {
+            try 
+            {
+                string query = Prompter.Prompt("Enter name: ",
+                    new Validator.Validator(),
+                    canCancel: true
+                ).ToLower();
+                ListBooks(Books.Where(b => b.Name?.Contains(query, StringComparison.CurrentCultureIgnoreCase) ?? false));
+            }
+            catch (PrompterCancelledException)
+            {
+
+            }
+        }
+
+        public void SearchByAuthor()
+        {
+            try 
+            {
+                string query = Prompter.Prompt("Enter author: ",
+                    new Validator.Validator(),
+                    canCancel: true
+                ).ToLower();
+                ListBooks(Books.Where(b => b.Author?.Contains(query, StringComparison.CurrentCultureIgnoreCase) ?? false));
+            }
+            catch (PrompterCancelledException)
+            {
+
+            }
+        }
+
+        static void ListBooks(IEnumerable<Book> books)
+        {
+            int count = 0;
+            foreach (Book book in books) 
+            {
+                count++;
+                Console.WriteLine(
+                    $"{count,5}. | {book.ISBN} | {book.Author} - {book.Name} | {book.Price}"
+                );
+            }
+            if (count == 0)
+            {
+                var color = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("There are no books");
+                Console.ForegroundColor = color;
+            }
+        }
+
         [GeneratedRegex(@"((978[\--– ])?[0-9][0-9\--– ]{10}[\--– ][0-9xX])|((978)?[0-9]{9}[0-9Xx])")]
         private static partial Regex isbnRegex();
     }
--- Project3/Project3/Program.cs
+++ Project3/Project3/Program.cs
@@ -16,18 +16,29 @@ namespace Project3
                     Console.WriteLine("Select an option: ");
                     Console.WriteLine("1. Add books");
                     Console.WriteLine("2. List all books");
+                    Console.WriteLine("3. Search by name");
+                    Console.WriteLine("4. Search by author");
                     Console.WriteLine("0. Exit");
                     switch 
                     (
                         Prompter.Prompt("Your choice: ", 
-                            new Validator.Validator().WholeNumber<int>().OneOf(1, 2, 0), 
+                            new Validator.Validator().WholeNumber<int>().OneOf(1, 2, 3, 4, 0), 
                             canThrow: true
                         )
                     ) 
                     {
                         case 1: 
                             library.PromptAddBook();
-                            return;
+                            break;
+                        case 2: 
+                            library.ListAllBooks();
+                            break;
+                        case 3: 
+                            library.SearchByName();
+                            break;
+                        case 4: 
+                            library.SearchByAuthor();
+                            break;
 
                         case 0: 
                             Console.WriteLine("Goodbye");
