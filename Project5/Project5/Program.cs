4e36e764cd81dfd57b90b659fcc27920a20f7e4c
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2025-03-14 22:21:17 +0700

    Project5 + 6

---
+++ Project5/Project5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+namespace Project5
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Thread thread1 = new Thread(() => DoTask(1, 2000));
+            Thread thread2 = new Thread(() => DoTask(2, 3000));
+            Thread thread3 = new Thread(() => DoTask(3, 1500));
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+
+            Console.WriteLine("All threads are completed");
+        }
+
+        static void DoTask(int number, int waitTime) 
+        {
+            Console.WriteLine($"Starting thread {number}");
+            Thread.Sleep(waitTime);
+            Console.WriteLine($"Thread {number} is completed");
+        }
+    }
+}
---
+++ Project6/Project6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+
+namespace Project6
+{
+    partial class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Task task1 = DoArithmeticTask(1, (a, b) => a + b, 10, 5, 2000);
+            Task task2 = DoArithmeticTask(2, (a, b) => a - b, 10, 5, 3000);
+            Task task3 = DoArithmeticTask(3, (a, b) => a * b, 10, 5, 1500);
+            Task task4 = DoArithmeticTask(4, (a, b) => a / b, 10, 5, 2500);
+
+            await Task.WhenAny(task1, task2, task3, task4);
+            Console.WriteLine("One task is completed");
+
+            await Task.WhenAll(task1, task2, task3, task4);
+            Console.WriteLine("All tasks are completed");
+        }
+
+        static async Task DoArithmeticTask(int number, Func<double, double, double> func, double a, double b, int waitTime) 
+        {
+            Console.WriteLine($"Doing task {number} with {a} and {b}");
+            double result = func(a, b);
+            await Task.Delay(waitTime);
+            Console.WriteLine($"Task {number} returned {result}");
+        }
+    }
+}
