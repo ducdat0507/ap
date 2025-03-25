cb4bd0828632170f0524c7e8ad2d760ae5a6abd2
aptechdoican285 <aptechdoican285@outlook.com>
2025-03-25 12:12:35 +0700

    a2

---
+++ RemoteCheckup/RemoteCheckup/SubServices/ProcessCheckupOnWindowsSubService.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Runtime.Versioning;
+using System.ServiceProcess;
+using System.Text.RegularExpressions;
+using RemoteCheckup.DTOs;
+using RemoteCheckup.Utilities;
+
+namespace RemoteCheckup.SubServices
+{
+    [SupportedOSPlatform("windows")]
+    public partial class ProcessCheckupOnWindowsSubService : ProcessCheckupSubService
+    {
+        public override void GetServicesInfo(ProcessesInfo info)
+        {
+            var servs = ServiceController.GetServices();
+            foreach (var serv in servs)
+            {
+                info.Services.Add(new ServiceInfo()
+                {
+                    Name = serv.DisplayName,
+                    Status = serv.Status switch
+                    {
+                        ServiceControllerStatus.Running => ServiceStatus.Running,
+                        ServiceControllerStatus.Stopped => ServiceStatus.Inactive,
+                        _ => ServiceStatus.Unknown,
+                    }
+                });
+            }
+        }
+    }
+}
